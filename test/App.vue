29c59be3dabac138d5030bffaa09a45ff29c009a
Luna <2650849-Lunalicious@users.noreply.gitlab.com>
2024-03-10 11:20:44 +0100

    Moved Files relevant for testing into their own directory

---
+++ test/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="test-wrapper">
+        <h2>Video Test</h2>
+        <luma-player src="test.mp4" poster-image="test.png" />
+        <h2>Audio Test</h2>
+        <luma-player src="test.wav" poster-image="test.png" />
+    </div>
+</template>
+
+<script setup>
+import LumaPlayer from "@/components/LumaPlayer.vue";
+</script>
+
+<style scoped>
+html, body {
+    margin: 0;
+    padding: 0;
+}
+
+.test-wrapper {
+    margin: 0 12px;
+}
+</style>
---
+++ test/main.js
@@ -0,0 +1,9 @@
+// Components
+import App from "./App.vue";
+
+// Composables
+import { createApp } from "vue";
+
+const app = createApp(App);
+
+app.mount("#app");
